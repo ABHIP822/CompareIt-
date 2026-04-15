038abb750b9d35370cf672f1eeaf94004f81f49e
ABHIP822 <ap7258616@gmail.com>
2026-04-15 13:54:11 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,30 +1,25 @@
-<script>
-    // പേജ് തുറക്കുമ്പോൾ തന്നെ വിവരങ്ങൾ അപ്ഡേറ്റ് ചെയ്യാൻ
-    window.onload = function() {
-        // LocalStorage-ൽ നിന്ന് വിവരങ്ങൾ എടുക്കുന്നു
-        const name = localStorage.getItem('userName') || "Not Set";
-        const age = localStorage.getItem('userAge') || "Not Set";
-        const gender = localStorage.getItem('userGender') || "Not Set";
-        const email = localStorage.getItem('userEmail') || "Not Set";
+window.onload = function() {
+    // ലോഗിൻ ചെയ്യുമ്പോൾ നിങ്ങൾ നൽകിയ കീ (Key) അനുസരിച്ച് ഇവിടെ മാറ്റം വരുത്തുക
+    const name = localStorage.getItem('userName') || "Not set";
+    const age = localStorage.getItem('userAge') || "Not set";
+    const gender = localStorage.getItem('userGender') || "Not set";
+    const email = localStorage.getItem('userEmail') || "Not set";
 
-        // പേജിലെ സ്പാനുകളിലേക്ക് വിവരങ്ങൾ നൽകുന്നു
-        document.getElementById('disp-name').innerText = name;
-        document.getElementById('disp-age').innerText = age;
-        document.getElementById('disp-gender').innerText = gender;
-        document.getElementById('disp-email').innerText = email;
+    // വിവരങ്ങൾ ഡിസ്‌പ്ലേ ചെയ്യുന്നു
+    document.getElementById('disp-name').innerText = name;
+    document.getElementById('disp-age').innerText = age;
+    document.getElementById('disp-gender').innerText = gender;
+    document.getElementById('disp-email').innerText = email;
 
-        // പേരിന്റെ ആദ്യ അക്ഷരം ഐക്കണിൽ കാണിക്കാൻ
-        if (name !== "Not Set") {
-            document.getElementById('initial').innerText = name.charAt(0).toUpperCase();
-        }
-    };
-
-    // ലോഗൗട്ട് ഫങ്ക്ഷൻ
-    function logout() {
-        if(confirm("ലോഗൗട്ട് ചെയ്യണോ?")) {
-            // വിവരങ്ങൾ വേണമെന്നുണ്ടെങ്കിൽ clear() ചെയ്യേണ്ടതില്ല
-            // നേരെ ലോഗിൻ പേജിലേക്ക് വിടാം
-            window.location.href = 'login.html';
-        }
+    // പേരിന്റെ ആദ്യ അക്ഷരം ഐക്കണിൽ മാറ്റുന്നു
+    if (name !== "Not set") {
+        document.getElementById('initial').innerText = name.charAt(0).toUpperCase();
     }
-</script>
+};
+
+function logout() {
+    if (confirm("Are you sure you want to log out?")) {
+        // ലോഗിൻ പേജിലേക്ക് പോകുന്നു
+        window.location.href = 'login.html';
+    }
+}
